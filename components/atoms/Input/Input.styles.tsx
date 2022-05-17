5a761f1b986f15ea93b8b10ba2386558991ee9b5
hyjoong <maplesosser@naver.com>
2022-05-17 15:43:21 +0900

    feat: Input컴포넌트 구현

---
+++ components/atoms/Input/Input.styles.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  height: 20px;
+  width: 550px;
+  padding: 10px;
+  outline: none;
+  border: 1px solid #000000;
+  ::placeholder {
+    color: grey;
+  }
+`;
---
+++ components/atoms/Input/Input.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import * as Styled from "./Input.styles";
+
+interface Props {
+  onChange: (e) => void;
+  onKeyUp: (e) => void;
+}
+
+const Input = ({ onChange, onKeyUp }: Props) => {
+  return (
+    <Styled.Input
+      onChange={onChange}
+      onKeyUp={onKeyUp}
+      placeholder="Github Repository를 검색하세요 "
+    ></Styled.Input>
+  );
+};
+
+export default Input;
