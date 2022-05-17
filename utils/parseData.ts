ad98fcc6644514dbc72edde5fea16ba4b20cd41a
hyjoong <maplesosser@naver.com>
2022-05-17 20:58:11 +0900

    feat: parseRepoDataList 유틸 함수 구현 - 이슈를 검색해서 받은 데이터 중에서 레포지토리의 1.계정 주인, 2. 레포지토리 이름 데이터만 parse후 반환

---
+++ utils/parseData.ts
@@ -0,0 +1,8 @@
+const parseRepoData = (repo: any) => {
+  const item = repo.full_name.split("/");
+  return { owner: item[0], repo: item[1] };
+};
+
+export const parseRepoDataList = (repo: []) => {
+  return repo.map(parseRepoData);
+};
