5b2efcf3dddb9893586585f81c2066a209f434ac
hyjoong <maplesosser@naver.com>
2022-05-19 12:38:15 +0900

    feat: getUserRepo api 구현

--- api/api.ts
+++ api/api.ts
@@ -11,7 +11,7 @@ export const api = axios.create({
 
 export const getIssuesApi = async (owner: string, repo: string) => {
   const response = await api(
-    `/${owner}/${repo}/issues?state=all&&per_page=100`
+    `/repos/${owner}/${repo}/issues?state=all&&per_page=100`
   );
   if (response.status !== 200) {
     throw response.status;
@@ -20,12 +20,20 @@ export const getIssuesApi = async (owner: string, repo: string) => {
 };
 
 export const searchRepoApi = async (repo: string) => {
-  const response = await api(
-    `/search/repositories?q=${repo}&per_page=20&page=1`
+  const response: any = await api(
+    `/search/repositories?q=${repo}&per_page=100&page=1`
   );
   if (response.status !== 200) {
     throw response.status;
   }
 
   return response.data.items;
+};
+
+export const getUserRepo = async (owner: string) => {
+  const response = await api(`/users/${owner}/repos?sort=updated&per_page=100`);
+  if (response.status !== 200) {
+    throw response.status;
+  }
+  return response.data;
 };
