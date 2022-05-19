c587da3296c24e29e728d075943dc49c65cc3ccf
hyjoong <maplesosser@naver.com>
2022-05-19 12:36:28 +0900

    feat: index 페이지 searchBar 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,53 @@
 import SearchBar from "components/molecules/SearchBar/SearchBar";
 import BaseLayout from "components/template/BaseLayout/BaseLayout";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useCallback, useState } from "react";
 
 const Index = () => {
-  const [input, setInput] = useState<string>("");
+  const [repoInput, setRepoInput] = useState<string>("");
+  const [userInput, setUserInput] = useState<string>("");
   const router = useRouter();
 
   const handleChange = (e) => {
-    setInput(e.target.value);
+    setRepoInput(e.target.value);
+  };
+  const handleUserChange = (e) => {
+    setUserInput(e.target.value);
   };
 
-  const handleSearch = async (input: string) => {
+  const handleRepoSearch = async (input: string) => {
     if (input === "") {
       alert("입력하세요");
       return;
     }
     router.push({
       pathname: `/Search`,
-      query: { keyword: input },
+      query: { keyword: input, limit: 20 },
     });
   };
 
-  const handleSubmit = () => {
-    handleSearch(input);
+  const handleUserSearch = async (input: string) => {
+    if (input === "") {
+      alert("입력하세요");
+      return;
+    }
+    router.push(`user/${input}`);
   };
 
   const onKeyUp = useCallback((e) => {
     const keyword = e.target.value;
+    console.log("etat", e.target.placeholder);
     if (e.keyCode === 13) {
       if (e.target.value.trim() === "") {
         alert("입력하세요");
         return;
       }
-      handleSearch(keyword);
+      if (e.target.placeholder === "Github Repository를 검색하세요") {
+        handleRepoSearch(keyword);
+      } else {
+        handleUserSearch(keyword);
+      }
     }
     // eslint-disable-next-line
   }, []);
@@ -41,11 +55,26 @@ const Index = () => {
   return (
     <BaseLayout>
       <SearchBar
-        onClick={handleSubmit}
+        onClick={() => handleRepoSearch(repoInput)}
         onChange={(e) => handleChange(e)}
         onKeyUp={onKeyUp}
         placeholder="Github Repository를 검색하세요"
       />
+      <SearchBar
+        onClick={() => handleUserSearch(userInput)}
+        onChange={(e) => handleUserChange(e)}
+        onKeyUp={onKeyUp}
+        placeholder="Github User 이름을 검색하세요"
+      />
+
+      <Link
+        href={{
+          pathname: "/userPage",
+          query: { keyword: userInput },
+        }}
+      >
+        <a>이동</a>
+      </Link>
     </BaseLayout>
   );
 };
